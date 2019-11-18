647de17ac963de032f3e59a87570d169b9bc0e68
Jakeh Clark <jakeh.clark@bestbuy.com>
2019-11-18 11:50:04 -0600

    minor changes related to module 2

--- src/test/java/com/oreilly/demo/DemoApplicationTests.java
+++ src/test/java/com/oreilly/demo/DemoApplicationTests.java
@@ -2,13 +2,44 @@ package com.oreilly.demo;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.text.NumberFormat;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	private ApplicationContext ctx;
+
+	@Autowired @Qualifier("defaultCurrencyFormat")
+	private NumberFormat nf;
+
+	@Test
+	public void defaultCurrency() {
+		double amount = 12345678.901234;
+		System.out.println(nf.format(amount));
+	}
+
+	@Test
+	public void germanCurrency() {
+		double amount = 12345678.901234;
+		NumberFormat deutschNF = ctx.getBean("germanCurrencyFormat", NumberFormat.class);
+		System.out.println(deutschNF.format(amount));
+	}
 
 	@Test
 	public void contextLoads() {
+		int count = ctx.getBeanDefinitionCount();
+		System.out.println("There are " + count + " beans in the application context");
+		for (String name : ctx.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
